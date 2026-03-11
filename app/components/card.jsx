262ffa4de45a4fef1aea6bb8ee07dfc0f85e7e3c
Yrieix-Cisterne-NWS <ycisterne@normandiewebschool.fr>
2026-03-11 15:12:14 +0100

    UI changes

--- app/components/card.jsx
+++ app/components/card.jsx
@@ -1,13 +1,19 @@
 import Link from "next/link"
 
-export default function Card({role, text}) {
+const colorRole = {
+    "tank": "text-[#00A2FF]",
+    "dps": "text-[#FF4655]",
+    "support": "text-[#40FFB3]"
+}
+
+export default function Card({role, text, color}) {
     return(
         <Link href={`/${role}`}>
         <div className="bg-[#1A2230] p-4 rounded-lg border-white/5 border-2 text-white">
             <h2 className="text-3xl mb-2 uppercase ">{role}</h2>
             <div className="flex flex-col gap-2">
                 <p className="text-slate-400">{text}</p>
-                <p className="text-slate-400">Explore stats</p>
+                <p className={colorRole[color]}>Explore stats</p>
             </div>
         </div>
         </Link>
--- app/page.jsx
+++ app/page.jsx
@@ -5,19 +5,20 @@ export default function Home() {
   return (
     <>
     <div className="flex flex-col items-center">
-      <div className="flex flex-col items-center">
-        <h1 className="text-white text-3xl">Stadium Cash generation</h1>
+      <div className="flex flex-col items-center pb-4">
+        <h1 className="text-white text-5xl">Stadium Cash generation</h1>
         <p className="text-white">This website group the cash generation of every heroes available in Stadium</p>
       </div>
-      <div>
+      <div className="flex flex-row gap-2 p-1 bg-[#00A2FF] mb-4">
         <h2 className="text-white">Any questions ?</h2>
         <a href="https://discord.gg/stadiumbuildsio" target="_blank" rel="noopener noreferrer" className="text-white">Join the SBIO Discord</a>
+        <img src="img/icons/discord.svg" alt="Discord" className="w-6 h-6"/>
       </div>
-      <div className="flex flex-col gap-4 items-center">
+      <div className="flex flex-col gap-8 items-center">
         <div className="flex flex-row gap-4">
-          <Card role="tank" text="Here's the cash generation for the Tanks in Stadium"/>
-          <Card role="dps" text="Here's the cash generation for the Dps in Stadium"/>
-          <Card role="support" text="Here's the cash generation for the Supports in Stadium"/>
+          <Card role="tank" text="Here's the cash generation for the Tanks in Stadium" color="tank"/>
+          <Card role="dps" text="Here's the cash generation for the Dps in Stadium" color="dps"/>
+          <Card role="support" text="Here's the cash generation for the Supports in Stadium" color="support"/>
         </div>
         <div className="flex flex-row gap-4">
           <Infocard title="What we discovered" items={["- Attack Speed, Weapon Power and Ability Power does not affect cash generation", "- Damage on tanks generate 40% less cash", "- Healing on tanks generate 30% less cash", "- Cash per damage is the same for healing", "- Overhealth does reduce cash generation (Thanks to Bird for the info)", "- Stadium Quickplay gives +25% cash per damage"]}/>
